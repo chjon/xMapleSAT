99d9c105c94a6caa24a855d0735b70ccab374da8
Jonathan Chung <jt2chung@uwaterloo.ca>
2022-09-30 12:34:15 -0400

    update license info

--- xMapleLCM/core/SolverER.h
+++ xMapleLCM/core/SolverER.h
@@ -1,5 +1,5 @@
 /*******************************************************************************************[SolverER.h]
-xMaple*, extended resolution for Minisat-based solvers -- Copyright (c) 2022, Jonathan Chung
+xMaple* -- Copyright (c) 2022, Jonathan Chung, Vijay Ganesh, Sam Buss
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
