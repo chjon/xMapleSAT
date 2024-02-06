bfa44c539a83619001c727e3b8e46b24f47b376a
oliveras <oliveras@cs.upc.edu>
2024-02-06 17:58:47 +0100

    Update

---
+++ xMapleLCM-DIP/README.txt
@@ -0,0 +1,7 @@
+Compilation:
+
+make xmaplelcm
+
+Or
+
+make xmaplelcm_debug (if we want assertions to be checked)
--- xMapleLCM-DIP/experiments/randomkxor/s.sh
+++ xMapleLCM-DIP/experiments/randomkxor/s.sh
@@ -1,16 +1,16 @@
-n=150
+n=20
 while [ $n -le 500 ]; do
     echo $n
     echo "KISSAT;CRYPTOMINISAT;XMAPLELCM"
     for i in {1..5}; do
-	cnfgen randkxor 3 $n $n -T or 2 > s.cnf
+	cnfgen randkxor 4 $n $n -T or 3 > s.cnf
 	/usr/bin/time -o "time.txt" -f "%e" --quiet ~/recerca/systems/kissat-master-15-sept/build/kissat --time=60 s.cnf > kissat.txt
 	kissat=`cat time.txt`
 	rm time.txt
 	/usr/bin/time -o "time.txt" -f "%e" --quiet cryptominisat5 --maxtime 60 s.cnf > cryptominisat.txt
 	crypto=`cat time.txt`
 	rm time.txt
-	/usr/bin/time -o "time.txt" -f "%e" --quiet ../bin/xmaplelcm -compute-dip -cpu-lim=60 s.cnf > xmaple.txt
+	/usr/bin/time -o "time.txt" -f "%e" --quiet ../../bin/xmaplelcm -cpu-lim=60 s.cnf > xmaple.txt
 	maple=`cat time.txt`
 	rm time.txt
 	echo -n $kissat
@@ -19,5 +19,5 @@ while [ $n -le 500 ]; do
 	echo -n ";"
 	echo $maple
     done
-    n=$((n+10))
+    n=$((n+5))
 done
